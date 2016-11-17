604a2a3ae36b5799dd0432787679af18c2c239a3
C++ <cplus@hs3159j.local>
2016-11-17 15:03:51 -0600

    Initial Commit

---
+++ Enigma/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Enigma
+//
+//  Created by C++ on 11/17/16.
+//  Copyright © 2016 C++. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
