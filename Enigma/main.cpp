7959d3d08b45dbe1117d9144c0b57b74404b0644
C++ <cplus@hs3159k.local>
2016-11-21 14:38:42 -0600

    started the the 1st setting for the enigma machine

--- Enigma/main.cpp
+++ Enigma/main.cpp
@@ -3,21 +3,22 @@
 #include <time.h>
 #include <string>
 using namespace std;
-void encode(char word[])
+void encode(char word[], int code1[])
 {
-    int random = rand() % 26 + 98;
-    for (int i =0; word[i] != NULL; i++)
+    for (int j = 0; j < 3; j++)
     {
-        if (word[i] == 'y' || word[i] == 'z')
-        {
-            word[i] = word[i] - 26;
-        }
-        word[i] = word[i] + 2;
+    if (code1[j] == 1)
+       {
+    for (int i =0; word[i] != NULL; i++)
+         {
+      
+        word[i] = word[i] + 6;
         cout<<word[i];
+         }
+       }
     }
-    cout<<(char)random<<endl;
 }
-void decode(char word[])
+void decode(char word[], int code1[])
 {
     int c = -1;
     for (int i = 0; word[i] != NULL; i++)
@@ -36,6 +37,12 @@ int main()
 {
     srand(time(NULL));
     char word[9000] = {' '};
+    int code1[3];
+    cout<<"Enter the setting"<<endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cin>>code1[i];
+    }
     string ed;
     cout<<"Would you like to encode or decode"<<endl;
     cin>>ed;
@@ -43,13 +50,15 @@ int main()
     {
         cout<<"What word would you like to encode"<<endl;;
         cin>>word;
-        encode(word);
+        encode(word,code1);
+        return 0;
     }
     if (ed.compare("encode") == 0)
     {
         cout<<"What word would you like to decode"<<endl;
         cin>>word;
-        decode(word);
+        decode(word,code1);
+        return 0;
     }
     return 0;
 }
