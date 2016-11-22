13520946f2c940f39ec19bdbb79a23eca11de271
C++ <cplus@hs3159k.local>
2016-11-22 14:26:46 -0600

    added '1'

--- Enigma/main.cpp
+++ Enigma/main.cpp
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <string>
 using namespace std;
-void encode(char word[], int code1[])
+void encode(char word[], int code1[], int three)
 {
     for (int j = 0; j < 3; j++)
     {
@@ -13,51 +13,67 @@ void encode(char word[], int code1[])
          {
       
         word[i] = word[i] + 6;
-        cout<<word[i];
+        if (three == 6)
+        {
+            cout<<word[i];
+        }
+             else
+             {
+                 three++;
+             }
          }
        }
     }
 }
-void decode(char word[], int code1[])
+void decode(char word[], int code1[], int three)
 {
-    int c = -1;
-    for (int i = 0; word[i] != NULL; i++)
+    for (int j = 0; j < 3; j++)
     {
-        if (word[i] == 'a' || word[i] == 'b')
+        if (code1[j] == 1)
         {
-            word[i] = word[i] + 26;
+            for (int i =0; word[i] != NULL; i++)
+            {
+                
+                word[i] = word[i] - 6;
+                if (three == 6)
+                {
+                    cout<<word[i];
+                }
+                else
+                {
+                    three++;
+                }
+            }
         }
-        c++;
-        word[i] = word[i] - 2;
     }
-    word[c] = ' ';
-    cout<<word<<endl;
 }
 int main()
 {
     srand(time(NULL));
     char word[9000] = {' '};
-    int code1[3];
+    int code1[3], three = 0;
+    string ed;
+    cout<<"Would you like to encode or decode"<<endl;
+    cin>>ed;
     cout<<"Enter the setting"<<endl;
     for (int i = 0; i < 3; i++)
     {
         cin>>code1[i];
     }
-    string ed;
-    cout<<"Would you like to encode or decode"<<endl;
-    cin>>ed;
    if (ed.compare("encode") == 0)
     {
         cout<<"What word would you like to encode"<<endl;;
         cin>>word;
-        encode(word,code1);
+        encode(word,code1,three);
+        cout<<endl;
         return 0;
     }
-    if (ed.compare("encode") == 0)
+    if (ed.compare("decode") == 0)
     {
         cout<<"What word would you like to decode"<<endl;
         cin>>word;
-        decode(word,code1);
+        decode(word,code1,three);
+        cout<<endl;
         return 0;
     }
     return 0;
