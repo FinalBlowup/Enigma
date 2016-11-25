809fafc52b128d6cafa8fe68baba3765ce9be638
BigRockFan <fastbiker703@gmail.com>
2016-11-24 22:02:32 -0600

    Finished a LOT of the encode/decode numbers
    Hey guys.. I finished all the numbers. So it works AMAZINGLY well on my computer, but thats only because my computer can cout any ASCII char so its actually really cool. But unfortunately, that is not the case on xCode. For this program to work successfully on xCode it requires more updates which we need to talk about. Right now that is the only issue; otherwise we are pretty much done unless you guys want to make any major changes. 
    Happy Thanksgiving!

--- Enigma/main.cpp
+++ Enigma/main.cpp
@@ -8,11 +8,83 @@ void encode(char word[], int code1[])
     int counter = 0;
     for (int j = 0; j < 3; j++)
     {
+        if (code1[j] == 0)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] + 27;
+                counter++;
+            }
+        }
         if (code1[j] == 1)
         {
             for (int i = 0; word[i] != NULL; i++)
             {
                 word[i] = word[i] + 6;
+                counter++;
+            }
+        }
+        if (code1[j] == 2)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] - 12;
+                counter++;
+            }
+        }
+        if (code1[j] == 3)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] + 66;
+                counter++;
+            }
+        }
+        if (code1[j] == 4)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] - 23;
+                counter++;
+            }
+        }
+        if (code1[j] == 5)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] + 38;
+                counter++;
+            }
+        }
+        if (code1[j] == 6)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] - 42;
+                counter++;
+            }
+        }
+        if (code1[j] == 7)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] - 49;
+                counter++;
+            }
+        }
+        if (code1[j] == 8)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] + 68;
+                counter++;
+            }
+        }
+        if (code1[j] == 9)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] + 53;
                 counter++;
             }
         }
@@ -24,17 +96,93 @@ void encode(char word[], int code1[])
 }
 void decode(char word[], int code1[])
 {
+    int counter = 0;
     for (int j = 0; j < 3; j++)
     {
-        if (code1[j] == 1)
+        if (code1[j] == 0)
         {
-            for (int i =0; word[i] != NULL; i++)
+            for (int i = 0; word[i] != NULL; i++)
             {
-                
-                word[i] = word[i] - 6;
-                
+                word[i] = word[i] - 27;
+                counter++;
             }
         }
+        if (code1[j] == 1)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] - 6;
+                counter++;
+            }
+        }
+        if (code1[j] == 2)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] + 12;
+                counter++;
+            }
+        }
+        if (code1[j] == 3)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] - 66;
+                counter++;
+            }
+        }
+        if (code1[j] == 4)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] + 23;
+                counter++;
+            }
+        }
+        if (code1[j] == 5)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] - 38;
+                counter++;
+            }
+        }
+        if (code1[j] == 6)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] + 42;
+                counter++;
+            }
+        }
+        if (code1[j] == 7)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] + 49;
+                counter++;
+            }
+        }
+        if (code1[j] == 8)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] - 68;
+                counter++;
+            }
+        }
+        if (code1[j] == 9)
+        {
+            for (int i = 0; word[i] != NULL; i++)
+            {
+                word[i] = word[i] - 53;
+                counter++;
+            }
+        }
+    }
+    for (int i = 0; word[i] != NULL; i++)
+    {
+        cout<<word[i];
     }
 }
 int main()
@@ -45,13 +193,14 @@ int main()
     string ed;
     cout<<"Would you like to encode or decode"<<endl;
     cin>>ed;
-    cout<<"Enter the setting"<<endl;
-    for (int i = 0; i < 3; i++)
-    {
-        cin>>code1[i];
-    }
+
     if (ed.compare("encode") == 0)
     {
+        cout<<"Enter the setting"<<endl;
+        for (int i = 0; i < 3; i++)
+        {
+            cin>>code1[i];
+        }
         cout<<"What word would you like to encode"<<endl;;
         cin>>word;
         encode(word,code1);
@@ -60,11 +209,20 @@ int main()
     }
     if (ed.compare("decode") == 0)
     {
+        cout<<"Enter the setting"<<endl;
+        for (int i = 0; i < 3; i++)
+        {
+            cin>>code1[i];
+        }
         cout<<"What word would you like to decode"<<endl;
         cin>>word;
         decode(word,code1);
         cout<<endl;
         return 0;
+    }
+    else
+    {
+        cout<<"Type encode or decode."<<endl;
     }
     return 0;
 }
