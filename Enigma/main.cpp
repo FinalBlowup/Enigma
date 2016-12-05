229e3f722b93e90d2da37c45e425d7babd9d7751
C++ <cplus@hs3159j.local>
2016-12-05 14:58:30 -0600

    Added the recursion for "Enter encode or Decode"

--- Enigma/main.cpp
+++ Enigma/main.cpp
@@ -3,46 +3,48 @@
 using namespace std; //Used for convenience of not typing 'std::' every time
 void encode(char[] ,int[]);//Encode function introduced
 void decode(char[] , int[]); //Decode function introduced
-
+void re(string, int[], char[]);
 int main()
 {
     char word[9000] = {' '}; //The word to be encoded
     int code1[3]; //Used for the settings
     string ed; //The 'encode' or 'decode' is saved in a string so that .compare can be used
-    cout<<"Would you like to encode or decode"<<endl;
+    re(ed, code1, word);
+}
+void re(string ed,int code1[],char word[])
+{
+    cout<<"Would you like to encode or decode? (Remember not to use numbers)"<<endl;
     cin>>ed; //User enters whether they want to encode or decode
     
     if (ed.compare("encode") == 0) //If the user enters encode, it enters the if statement
     {
-        cout<<"Enter the setting (Three numbers between 0 and 9)"<<endl;
+        cout<<"Enter the setting (Three numbers between 0 and 9)."<<endl;
         for (int i = 0; i < 3; i++) //For loop to run thrice and get three settings into the array
         {
             cin>>code1[i]; //User enters three numbers between 0 and 9 for the setting
         }
-        cout<<"What would you like to encode (only letters and underscore)"<<endl; //User should only enter letters or underscore
+        cout<<"What would you like to encode? (Only letters and underscore)"<<endl; //User should only enter letters or underscore
         cin>>word; //User enters the word that they would like to encode
         encode (word,code1); //Runs the function to encode the word
         cout<<endl;
-        return 0;
     }
     else if (ed.compare("decode") == 0) //If the user enters decode, it enters the other if statement
     {
-        cout<<"Enter the setting (Three numbers between 0 and 9"<<endl;
+        cout<<"Enter the setting (Three numbers between 0 and 9)."<<endl;
         for (int i = 0; i < 3; i++) //For loop to run thrice and get three settings into the array
         {
             cin>>code1[i]; //User enters three numbers between 0 and 9 for the setting
         }
-        cout<<"What would you like to decode (only letters and underscore)"<<endl; //User should only enter letters or underscore
+        cout<<"What would you like to decode? (Only letters and underscore)"<<endl; //User should only enter letters or underscore
         cin>>word; //User enters the word that they would like to decode
         decode(word,code1); //Runs the function to decode the word
         cout<<endl;
-        return 0;
     }
-    else //If the user enters anything other than 'encode' or 'decode', the program will tell them to enter appropriately and end the program
+    else
     {
-        cout<<"Type encode or decode."<<endl;
+        cout<<"Try again, you failed, enter 'encode' or 'decode', now: "<<endl;
+        re(ed, code1, word);
     }
-    return 0;//Returns 0
 }
 void encode (char word[], int code1[]) //Encode function
 {
