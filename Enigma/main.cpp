fd8674e62eb41939ccd7ed8caaa41e226607246c
C++ <cplus@hs3159j.local>
2016-12-05 14:31:27 -0600

    Fixed setting problem.

--- Enigma/main.cpp
+++ Enigma/main.cpp
@@ -14,7 +14,7 @@ int main()
     
     if (ed.compare("encode") == 0) //If the user enters encode, it enters the if statement
     {
-        cout<<"Enter the setting"<<endl;
+        cout<<"Enter the setting (Three numbers between 0 and 9)"<<endl;
         for (int i = 0; i < 3; i++) //For loop to run thrice and get three settings into the array
         {
             cin>>code1[i]; //User enters three numbers between 0 and 9 for the setting
@@ -27,7 +27,7 @@ int main()
     }
     else if (ed.compare("decode") == 0) //If the user enters decode, it enters the other if statement
     {
-        cout<<"Enter the setting"<<endl;
+        cout<<"Enter the setting (Three numbers between 0 and 9"<<endl;
         for (int i = 0; i < 3; i++) //For loop to run thrice and get three settings into the array
         {
             cin>>code1[i]; //User enters three numbers between 0 and 9 for the setting
@@ -53,72 +53,176 @@ void encode (char word[], int code1[]) //Encode function
             case 0: //If any of the user's settings is 0 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] + 4;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] + 4;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] + 1;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] - 9;
+                    }
                 }
                 break;
             case 1: //If any of the user's settings is 1 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] + 1;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] + 1;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] + 4;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] - 6;
+                    }
                 }
                 break;
             case 2:  //If any of the user's settings is 2 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++) //If any of the user's settings is 2 then the following changes in the for loop occur
                 {
-                    word[i] = word[i] - 9;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] - 9;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] - 7;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] + 3;
+                    }
                 }
                 break;
             case 3: //If any of the user's settings is 3 then the following changes in the for loop occur
-            
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] - 5;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] - 5;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] - 2;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] + 1;
+                    }
                 }
                 break;
             case 4: //If any of the user's settings is 4 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] - 10;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] - 10;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] - 6;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] - 3;
+                    }
+ 
                 }
                 break;
             case 5: //If any of the user's settings is 5 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] - 3;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] - 3;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] + 3;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] + 4;
+                    }
                 }
                 break;
             case 6: //If any of the user's settings is 6 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] - 8;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] - 8;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] - 4;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] - 10;
+                    }
+ 
                 }
                 break;
             case 7: //If any of the user's settings is 7 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] - 4;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] - 4;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] + 2;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] - 5;
+                    }
+
                 }
                 break;
             case 8: //If any of the user's settings is 8 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] + 2;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] + 2;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] - 9;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] - 1;
+                    }
+
                 }
                 break;
             case 9: //If any of the user's settings is 9 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] - 6;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] - 6;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] - 3;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] + 2;
+                    }
+
                 }
                 break;
             default:
@@ -139,71 +243,171 @@ void decode(char word[], int code1[]) //Decode function
             case 0: //If any of the user's settings is 0 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] - 4;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] - 4;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] - 1;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] + 9;
+                    }
                 }
                 break;
             case 1: //If any of the user's settings is 1 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] - 1;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] - 1;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] - 4;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] + 6;
+                    }
                 }
                 break;
             case 2: //If any of the user's settings is 2 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] + 9;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] + 9;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] + 7;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] - 3;
+                    }
                 }
                 break;
             case 3: //If any of the user's settings is 3 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] + 5;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] + 5;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] + 2;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] - 1;
+                    }
                 }
                 break;
             case 4: //If any of the user's settings is 4 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] + 10;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] + 10;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] + 6;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] + 3;
+                    }
                 }
                 break;
             case 5: //If any of the user's settings is 5 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] + 3;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] + 3;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] - 3;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] - 4;
+                    }
                 }
                 break;
             case 6: //If any of the user's settings is 6 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] + 8;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] + 8;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] + 4;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] + 10;
+                    }
                 }
                 break;
             case 7: //If any of the user's settings is 7 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] + 4;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] + 4;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] - 2;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] + 5;
+                    }
                 }
                 break;
             case 8: //If any of the user's settings is 8 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] - 2;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] - 2;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] + 9;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] + 1;
+                    }
                 }
                 break;
             case 9: //If any of the user's settings is 9 then the following changes in the for loop occur
                 for (int i = 0; word[i] != NULL; i++)
                 {
-                    word[i] = word[i] + 6;
-                    
+                    if (j == 0)
+                    {
+                        word[i] = word[i] + 6;
+                    }
+                    else if (j == 1)
+                    {
+                        word[i] = word[i] + 3;
+                    }
+                    else if (j == 2)
+                    {
+                        word[i] = word[i] - 2;
+                    }
                 }
                 break;
             default:
@@ -215,4 +419,3 @@ void decode(char word[], int code1[]) //Decode function
         cout<<word[i];
     }
 }
-
